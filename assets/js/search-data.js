8e92a4590f3b8853b6350e368aba2453b9b6cb76
satalab <ryanseminl1@gmail.com>
2025-12-28 10:00:36 +0000

    Deploying to gh-pages from @ satalab/satalab.github.io@1a4a6655d50bf4a454ed4d81bdaa93e3c1d21b6b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-members",
           title: "Members",
-          description: "members of the lab or group",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/people/";
