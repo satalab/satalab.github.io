ccfa6098ba2c67420732895a4d7d5227d6aa656e
satalab <ryanseminl1@gmail.com>
2026-01-03 05:07:39 +0000

    Deploying to gh-pages from @ satalab/satalab.github.io@01abbe3ced72390c26b82b84e8c8a3f6dbae9a61 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -126,9 +126,9 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-disaggregated-systems",
+          title: 'Disaggregated Systems',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
