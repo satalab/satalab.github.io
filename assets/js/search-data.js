87231b404f62f843aa7f3353f53b451eeffe9545
satalab <ryanseminl1@gmail.com>
2026-05-17 03:13:16 +0000

    Deploying to gh-pages from @ satalab/satalab.github.io@c3916ff8b14ad76961f39e5980c48ce10fe479ab 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,7 +44,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/research/";
           },
-        },{id: "post-our-paper-has-been-accepted-to-isca",
+        },{id: "post-welcome-aboard-seungkook",
+        
+          title: "Welcome aboard, Seungkook!",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/news/2026/lab_7/";
+          
+        },
+      },{id: "post-our-paper-has-been-accepted-to-isca",
         
           title: "Our paper has been accepted to ISCA!",
         
